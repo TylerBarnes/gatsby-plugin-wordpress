48d5405694399258aa6e8e8212195dc57d816049
Tyler Barnes <tylerdbarnes@gmail.com>
2020-06-18 09:07:54 -0700

    add comment

--- src/hooks/templates.js
+++ src/hooks/templates.js
@@ -1,5 +1,10 @@
 import { addNodeFilter } from "gatsby-source-wordpress-experimental/hooks"
 
+/**
+ * This filter adds a directory to the list of directories this plugin
+ * will look in to find template-hierarchy templates
+ * @param {string} directory
+ */
 export const addTemplateDirectory = (directory) => {
   addNodeFilter({
     name: `wp-template-directories`,
