8e7dfd44026339b4fbb86ea447d0b9bf6850264d
Tyler Barnes <tylerdbarnes@gmail.com>
2020-07-16 14:10:38 -0700

    update browser helper logs and allow turning them off via plugin options

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -3,18 +3,47 @@
 const logStyles =
   "font-size: 15px; font-weight: bold; padding: 10px 20px; background: black; color: white;"
 
+/**
+ * This export adds helper messages to the browser console in development
+ * It can be turned off by setting the plugin option browserConsoleHelp to false.
+ * It's enabled by default
+ */
 exports.wrapPageElement =
   process.env.NODE_ENV === `development`
-    ? ({ element, props }) => {
+    ? ({ element, props }, { browserConsoleHelp = true }) => {
+        if (!browserConsoleHelp) {
+          console.log(
+            `[gatsby-plugin-wordpress] add { options: { browserConsoleHelp: true } } to your plugin options in gatsby-config.js for help with this page`
+          )
+          return element
+        }
+
+        const {
+          pageContext: { isArchive, isSingle },
+        } = props
+        const pageTypeString =
+          isArchive && isSingle
+            ? `archive/single`
+            : isArchive
+            ? `archive`
+            : `single`
+
         console.log(
-          `%c🚀[gatsby-plugin-wordpress] The template used for this page is:\n${props.pageContext.component__DEVELOPMENT_ONLY}`,
+          `%c🚀[gatsby-plugin-wordpress] The template used for this ${pageTypeString} page is:\n${props.pageContext.component__DEVELOPMENT_ONLY}`,
           logStyles
         )
         console.log(
-          "%c🚀[gatsby-plugin-wordpress] These are the page props for this path:",
+          `%c🚀[gatsby-plugin-wordpress] props.data is the result of the GraphQL query for this ${pageTypeString} page:`,
           logStyles
         )
-        console.log(props)
+        console.log(props.data)
+        console.log(
+          `%c🚀[gatsby-plugin-wordpress] props.pageContext contains the variables passed to your page component and GraphQL query for this page:`,
+          logStyles
+        )
+        delete props.pageContext.component__DEVELOPMENT_ONLY
+        console.log(props.pageContext)
+
         return element
       }
     : false
